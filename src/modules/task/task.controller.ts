303091f8c88366e84e975ce852ad41ec5848c59d
Milan Kumar Bhagat <milankrbhagat@gmail.com>
2026-04-03 17:12:05 +0530

    Auth completion

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('tasks')
+@UseGuards(JwtAuthGuard) // 🔐 PROTECTED
 export class TaskController {
     constructor(private taskService: TaskService) { }
 
